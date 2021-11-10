8e2b987e22dd40929e2dfba9f00e4898d38a4357
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-11-10 10:15:54 +0100

    Added formatter option to use spaces

--- src/itertools/FuncIterable.hx
+++ src/itertools/FuncIterable.hx
@@ -4,12 +4,12 @@ package itertools;
     An iterable based on a function to create the iterator.
 **/
 class FuncIterable<T> {
-	final f:() -> Iterator<T>;
+    final f:() -> Iterator<T>;
 
-	public function new(f) {
-		this.f = f;
-	}
+    public function new(f) {
+        this.f = f;
+    }
 
-	public function iterator()
-		return this.f();
+    public function iterator()
+        return this.f();
 }
